410216637ea47a2d22bca3b308a6486858d46ecc
aklein <allarklein@gmail.com>
2025-09-19 17:42:41 +0300

    feat: add Nix flake configuration for development environment

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "Tool Tracker Go Development Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go # Latest Go version
+            gopls # Go language server
+            gotools # Additional Go tools (goimports, etc.)
+            air # Hot reload tool (since you're using it)
+            docker
+            docker-compose
+            # Development tools
+            git
+            curl
+            jq
+          ];
+
+          shellHook = ''
+            echo "Tool Tracker development environment loaded!"
+            echo "Go version: $(go version)"
+            echo "Available commands: go, air, docker, docker-compose"
+
+            # Set up Go environment
+            export GOPATH="$PWD/.go"
+            export GOCACHE="$PWD/.go/cache"
+            mkdir -p "$GOPATH" "$GOCACHE"
+
+            # VS Code will inherit these environment variables
+            echo "GOPATH set to: $GOPATH"
+          '';
+        };
+      }
+    );
+}
